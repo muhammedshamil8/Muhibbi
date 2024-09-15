9560b4e320869fa39c3b2912c1a27ec850d25be6
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-15 17:30:28 +0530

    refactor(Captain): enhance component structure

--- src/pages/Home/components/Captain/index.jsx
+++ src/pages/Home/components/Captain/index.jsx
@@ -1,5 +1,6 @@
 import { boy, girl } from "@/assets/images";
 import classNames from 'classnames';
+import { motion } from "framer-motion"
 
 function Captain() {
   const Teams = [
@@ -34,46 +35,53 @@ function Captain() {
       <h2 className="text-2xl md:text-4xl font-bold my-10">Captains</h2>
       <div className="flex justify-around w-full my-10 flex-col md:flex-row gap-14 md:gap-2" id="captains">
         {Teams.map((team, index) => (
-          <div
+          <motion.div
             key={index}
-            className={classNames(`border-2  p-4 rounded-lg min-w-[230px] max-w-[300px] mx-auto`,
-              {
-                'border-teal-500': team.color === 'teal',
-                'border-amber-500': team.color === 'amber',
-                'border-purple-500': team.color === 'purple'
-              })}
-          >
-            <h3
-              className={classNames(`font-semibold text-lg  text-white p-2 rounded-lg text-center -mt-10 mb-6`,
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2, delay: index * 0.1 }}
+            viewport={{ once: true }} >
+            <div
+              key={index}
+              className={classNames(`border-2  p-4 rounded-lg min-w-[230px] max-w-[300px] mx-auto`,
                 {
-                  'bg-teal-700': team.color === 'teal',
-                  'bg-orange-500': team.color === 'amber',
-                  'bg-purple-900': team.color === 'purple'
+                  'border-teal-500': team.color === 'teal',
+                  'border-amber-500': team.color === 'amber',
+                  'border-purple-500': team.color === 'purple'
                 })}
             >
-              {team.name}
-            </h3>
-            <div>
-              {team.captains.map((captain,index) => (
-                <div key={index}
-                  className={classNames(`flex items-center mb-2 text-white  p-2 rounded-lg`,
-                    {
-                      'bg-teal-700': team.color === 'teal',
-                      'bg-orange-500': team.color === 'amber',
-                      'bg-purple-900': team.color === 'purple'
-                    })}>
-                  <div className="bg-white rounded-lg mr-2">
-                    <img
-                      src={captain.gender === "male" ? boy : girl}
-                      alt={captain.name}
-                      className="w-10 h-10"
-                    />
+              <h3
+                className={classNames(`font-semibold text-lg  text-white p-2 rounded-lg text-center -mt-10 mb-6`,
+                  {
+                    'bg-teal-700': team.color === 'teal',
+                    'bg-orange-500': team.color === 'amber',
+                    'bg-purple-900': team.color === 'purple'
+                  })}
+              >
+                {team.name}
+              </h3>
+              <div>
+                {team.captains.map((captain, index) => (
+                  <div key={index}
+                    className={classNames(`flex items-center mb-2 text-white  p-2 rounded-lg`,
+                      {
+                        'bg-teal-700': team.color === 'teal',
+                        'bg-orange-500': team.color === 'amber',
+                        'bg-purple-900': team.color === 'purple'
+                      })}>
+                    <div className="bg-white rounded-lg mr-2">
+                      <img
+                        src={captain.gender === "male" ? boy : girl}
+                        alt={captain.name}
+                        className="w-10 h-10"
+                      />
+                    </div>
+                    <span className="font-semibold">{captain.name}</span>
                   </div>
-                  <span className="font-semibold">{captain.name}</span>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
